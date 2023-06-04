f8d633523f0b12677f914d66cf0e7452200ff20c
duynt2499 <118056890+duynt99@users.noreply.github.com>
2023-06-04 20:00:57 +0700

    update title of application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,35 +10,40 @@ export default function Home() {
   const { listInfoAddress, isLoading } = useGlobalData();
 
   return (
-    <main className="flex flex-col items-center justify-start p-16">
-      <h1 className="text-4xl font-mono">Wallets checking</h1>
-      <div className="mt-6 w-full max-w-3xl flex justify-end">
-        {isLoading ? (
-          <div className="py-2 w-[100px] rounded-sm text-black transition-all duration-300 flex cursor-not-allowed bg-gray-600 justify-center items-center">
-            <Spinner />
-          </div>
-        ) : (
-          <CSVLink
-            data={formatExportData(listInfoAddress)}
-            className="py-2 px-2  rounded-sm text-black transition-all duration-300 flex cursor-pointer bg-white hover:bg-opacity-70"
-            filename={exportFileName}
-          >
-            <DocumentArrowDownIcon className="w-6 h-6 mr-2" />
-            <p>Export CSV</p>
-          </CSVLink>
-        )}
-      </div>
-      <div className="mt-6 w-full max-w-3xl">
-        {isLoading && (
-          <div className="flex justify-center">
-            <Spinner />
-          </div>
-        )}
-        {!isLoading &&
-          listInfoAddress.map((item) => {
-            return <CardItem key={item.address} item={item} />;
-          })}
-      </div>
-    </main>
+    <>
+      <head>
+        <title>Wallets checking</title>
+      </head>
+      <main className="flex flex-col items-center justify-start p-16">
+        <h1 className="text-4xl font-mono">Wallets checking</h1>
+        <div className="mt-6 w-full max-w-3xl flex justify-end">
+          {isLoading ? (
+            <div className="py-2 w-[100px] rounded-sm text-black transition-all duration-300 flex cursor-not-allowed bg-gray-600 justify-center items-center">
+              <Spinner />
+            </div>
+          ) : (
+            <CSVLink
+              data={formatExportData(listInfoAddress)}
+              className="py-2 px-2  rounded-sm text-black transition-all duration-300 flex cursor-pointer bg-white hover:bg-opacity-70"
+              filename={exportFileName}
+            >
+              <DocumentArrowDownIcon className="w-6 h-6 mr-2" />
+              <p>Export CSV</p>
+            </CSVLink>
+          )}
+        </div>
+        <div className="mt-6 w-full max-w-3xl">
+          {isLoading && (
+            <div className="flex justify-center">
+              <Spinner />
+            </div>
+          )}
+          {!isLoading &&
+            listInfoAddress.map((item) => {
+              return <CardItem key={item.address} item={item} />;
+            })}
+        </div>
+      </main>
+    </>
   );
 }
